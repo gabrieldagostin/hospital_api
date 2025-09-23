e336f1a3adf0e58fbdc6967e72dd1681c9c8ac8e
Gabriel Fernandes Dagostin <gabrielfernandes140608@gmail.com>
2025-09-23 16:34:33 -0300

    fix(Paciente): patient atualization no more accepts blank patterns

--- src/main/java/com/hospital/mateus/curso/paciente/model/Paciente.java
+++ src/main/java/com/hospital/mateus/curso/paciente/model/Paciente.java
@@ -34,7 +34,7 @@ public class Paciente extends Pessoa {
     private Medico medico;
 
     public void atualizarInformacoes(DadosAtualizarPaciente dados) {
-        if (dados.nome() != null && !Objects.equals(this.nome, dados.nome())) {
+        if (dados.nome() != null && !dados.nome().isBlank() && !Objects.equals(this.nome, dados.nome())) {
             this.nome = dados.nome();
         }
     }
--- target/generated-sources/annotations/com/hospital/mateus/curso/paciente/mapper/PacienteMapperImpl.java
+++ target/generated-sources/annotations/com/hospital/mateus/curso/paciente/mapper/PacienteMapperImpl.java
@@ -14,8 +14,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-02T11:26:15-0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 24.0.2 (Oracle Corporation)"
+    date = "2025-09-23T16:01:28-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 25 (Oracle Corporation)"
 )
 @Component
 public class PacienteMapperImpl implements PacienteMapper {
